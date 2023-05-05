1f6bffd0ba808b4a15dea34417b879a3dae98fd7
Adam Sori <adam.sori@gmail.com>
2023-05-05 00:38:19 -0300

    Controls for the car

--- car.js
+++ car.js
@@ -4,12 +4,19 @@ class Car {
         this.y = y;
         this.width = width;
         this.height = height;
+
+        this.controls = new Controls();
     }
 
+    // Draw a car
     draw(ctx) {
         ctx.beginPath();
         ctx.rect(
-            this
-        )
+            this.x - this.width / 2,
+            this.y - this.height / 2,
+            this.width,
+            this.height
+        );
+        ctx.fill();
     }
 }
